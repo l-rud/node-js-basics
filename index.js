f39aafd4d9c50af6b724652b4dd88bc246163ea9
Larysa Rud <larpavrud@gmail.com>
2024-05-22 17:57:21 -0400

    Changing the response sent by the server when a request is received

--- index.js
+++ index.js
@@ -17,18 +17,40 @@ const port = 3000;
 The configuration below respond with a status code of 200 (“success”) and the text content “Hello World!”
 The variables req and res correspond to the request and response objects of the server communications. */
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-  });
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World!\n');
+//   });
 
 /*Finally, we need to tell the server to actually listen for communications. 
 We do this by calling the listen method of the server object we just created, 
 and passing it the port and hostname we previously defined. 
 It also accepts a callback function that we can use to indicate that the server is running. */
 
+//   server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+//   });
+
+//Change our createServer call to the following:
+
+/*The Content-Type header is set to text/html, indicating that the response body will be in HTML format.
+Instead of sending plain text (res.end('Hello World!\n');), 
+the response body now consists of HTML content.
+The HTML content includes an <h1> heading with red color and a <p> paragraph.
+When a client makes a request to this server, it will respond with an HTML page 
+containing the heading and paragraph specified in the script.
+So, instead of just sending a plain text "Hello World!", 
+the server will now send an HTML response with formatted content.*/
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.end();
+  }); 
+
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
-  
